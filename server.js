6731006ad81893b2c49103654c49b35216d0ed27
jteng <akariass@gmail.com>
2018-08-08 14:50:06 -0400

    fixed app name

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 
 const app = express();
 
-// Serve only the static files from the dist directory
-app.use(express.static('./dist/app-root'));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/webdev-summer2-client-angular-jteng'));
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/dist/app-root/index.html'));
+    res.sendFile(path.join(__dirname, '/dist/webdev-summer2-client-angular-jteng/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
