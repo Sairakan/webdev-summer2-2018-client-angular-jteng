2126f0aa5c20143a4118276a28efd048bce82b1b
jteng <akariass@gmail.com>
2018-08-08 14:17:08 -0400

    deployment

--- src/app/services/const-url.ts
+++ src/app/services/const-url.ts
@@ -1,5 +1,5 @@
-export const NODE = 'http://localhost:4000';
-// export const NODE = 'http://';
+// export const NODE = 'http://localhost:4000';
+export const NODE = 'https://webdev-server-node-jteng.herokuapp.com';
 
-export const HOST = 'http://localhost:8080';
-// export const HOST = 'http://';
+// export const HOST = 'http://localhost:8080';
+export const HOST = 'https://webdev-server-java-jteng.herokuapp.com';
